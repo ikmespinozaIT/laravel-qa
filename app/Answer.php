e3f777a6fbd542843eb8e0b3960f88986c9c86db
Ian Kris Espinoza <ian.espinoza@naffco.com>
2019-01-08 09:47:28 +0400

    Implements delete functionality for answer

--- app/Answer.php
+++ app/Answer.php
@@ -35,11 +35,14 @@ class Answer extends Model
 
         static::created(function($answer) {
             $answer->question->increment('answers_count');
-            $answer->question->save();
         });
 
         static::saved(function($answer) {
             echo "Answer saved\n";
+        });
+
+        static::deleted(function($answer) {
+            $answer->question->decrement('answers_count');
         });
     }
 }
